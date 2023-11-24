00055b3b0483d21bdead8df325482a9b32a13dd5
timok <timokaalma@gmail.com>
2023-11-24 08:58:22 +0200

    NewFont

--- MauiPlanets/MauiProgram.cs
+++ MauiPlanets/MauiProgram.cs
@@ -9,9 +9,10 @@ public static class MauiProgram
 			.UseMauiApp<App>()
 			.ConfigureFonts(fonts =>
 			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
+				fonts.AddFont("Montserrat-Medium.ttf", "RegularFont");
+				fonts.AddFont("Montserrat-Semibold.ttf", "MediumFont");
+                fonts.AddFont("Montserrat-Bold.ttf", "BoldFont");
+            });
 
 		return builder.Build();
 	}
